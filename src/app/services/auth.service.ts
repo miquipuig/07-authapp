93270888475cbe221781edbaa4ee168bf42f2965
Miquel Puig <mpuig22@gmail.com>
2019-05-07 17:51:58 +0200

    profile

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,6 +7,8 @@ import * as auth0 from 'auth0-js';
 @Injectable()
 export class AuthService {
 
+  public userProfile:any;
+
   private _idToken: string;
   private _accessToken: string;
   private _expiresAt: number;
@@ -16,7 +18,7 @@ export class AuthService {
     domain: 'adela.eu.auth0.com',
     responseType: 'token id_token',
     redirectUri: 'http://localhost:4200/callback',
-    scope: 'openid'
+    scope: 'openid profile'
   });
 
   constructor(public router: Router) {
@@ -89,6 +91,19 @@ export class AuthService {
   }
 
 
+  public getProfile(cb): void {
+    if (!this._accessToken) {
+      throw new Error('Access Token must exist to fetch profile');
+    }
+  
+    const self = this;
+    this.auth0.client.userInfo(this._accessToken, (err, profile) => {
+      if (profile) {
+        self.userProfile = profile;
+      }
+      cb(err, profile);
+    });
+  }
 
 
 }
